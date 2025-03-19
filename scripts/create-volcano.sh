23b4666b3f071e1e0367ad8daaf0d59508c6b259
Andy Prowse <aprowse@starless.highvail.com>
2025-03-19 11:42:48 -0400

    updated on 2025-03-19 11:42

--- scripts/create-volcano.sh
+++ scripts/create-volcano.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-VM_PREFIX="cp-"  VM_NETWORK=default NUMOFVMS=3 VM_CORES=4 VM_MEMORY=8192                ./elemental-create.sh /export/iso/fire-nodes-2025-03-18T18_31_56Z.iso
+: ${ISO:="fire-nodes-2025-03-19T13_57_15Z.iso"}
+IMG=/export/iso/$ISO
 
-VM_PREFIX="wrk-" VM_NETWORK=default NUMOFVMS=3 VM_CORES=2 VM_MEMORY=4096 VM_DISKSIZE=50 ./elemental-create.sh /export/iso/fire-nodes-2025-03-18T18_31_56Z.iso
+VM_PREFIX="cp-"  VM_NETWORK=default NUMOFVMS=3 VM_CORES=4 VM_MEMORY=8192                ./elemental-create.sh $IMG
+VM_PREFIX="wrk-" VM_NETWORK=default NUMOFVMS=3 VM_CORES=2 VM_MEMORY=4096 VM_DISKSIZE=50 ./elemental-create.sh $IMG
