d7428306cd3e0df7d0353913af23c58a6fdc4aed
Andy Prowse <aprowse@starless.highvail.com>
2025-03-20 15:05:11 -0400

    updated on 2025-03-20 15:05

--- scripts/create-volcano.sh
+++ scripts/create-volcano.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+: ${CP:=1}
+: ${WRK:=1}
 : ${ISO:="fire-nodes-2025-03-20T12_08_23Z.iso"}
 IMG=/export/iso/$ISO
 
-VM_PREFIX="cp-"  VM_NETWORK=default NUMOFVMS=3 VM_CORES=4 VM_MEMORY=8192                ./elemental-create.sh $IMG
-VM_PREFIX="wrk-" VM_NETWORK=default NUMOFVMS=3 VM_CORES=2 VM_MEMORY=4096 VM_DISKSIZE=50 ./elemental-create.sh $IMG
+VM_PREFIX="cp-"  \
+  VM_NETWORK=default \
+  NUMOFVMS=$CP \
+  VM_CORES=4 \
+  VM_MEMORY=8192 \
+  ./elemental-create.sh $IMG
+
+VM_PREFIX="wrk-" \
+  VM_NETWORK=default \
+  NUMOFVMS=$WRK \
+  VM_CORES=2 \
+  VM_MEMORY=4096 \
+  VM_DISKSIZE=50 \
+  ./elemental-create.sh $IMG
--- scripts/elemental-cleanup.sh
+++ scripts/elemental-cleanup.sh
@@ -2,7 +2,11 @@
 
 VIRSH="virsh --connect qemu:///system"
 
-for DOMAIN in `$VIRSH list --title | awk '/elemental/ { print $2 }'x `; do
+for DOMAIN in `$VIRSH list --title | awk '/elemental/ { print $2 }'`; do
   $VIRSH destroy $DOMAIN
+  $VIRSH undefine --remove-all-storage --nvram --domain  ${DOMAIN}
+done
+
+for DOMAIN in `$VIRSH list --all --title | awk '/elemental/ { print $2 }'`; do
   $VIRSH undefine --remove-all-storage --nvram --domain  ${DOMAIN}
 done
